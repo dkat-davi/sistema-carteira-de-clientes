6b8610ccc4b7d86b8475c3ca95f62d4c033c0706
dkat <dkat.dev@gmail.com>
2026-06-28 20:01:28 -0300

    chore: cria estrutura inicial do projeto

---
+++ src/main/java/br/com/daviteixeira/carteiraclientes/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.daviteixeira.carteiraclientes;
+
+/**
+ *
+ * @author dkat
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Carteira de clientes");
+    }
+}
